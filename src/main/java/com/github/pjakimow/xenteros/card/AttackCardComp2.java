0db99b92d4c9da320dedc9e653248d50cce5bf6c
Patrycja Jakimów <patrycja.jakimow@gmail.com>
2018-03-24 09:41:10 +0100

    edit agressive player comparator

--- src/main/java/com/github/pjakimow/xenteros/card/AttackCardComp2.java
+++ src/main/java/com/github/pjakimow/xenteros/card/AttackCardComp2.java
@@ -17,15 +17,19 @@ public class AttackCardComp2 implements Comparator<Card> {
 								((Monster)o1).getAttack() > ((Monster)o2).getAttack() ? -1 : 1;
 					}
 				}
+				if ( ((Monster)o2).getMonsterAbility() == MonsterAbility.CHARGE ){
+					return ((Monster)o2).getAttack() == ((Monster)o1).getAttack() ? (((Monster)o2).getCost() < ((Monster)o1).getCost() ? 1 : -1) :
+							((Monster)o2).getAttack() > ((Monster)o1).getAttack() ? 1 : -1;
+				}
 			} else {
 				return ((Monster)o1).getMonsterAbility() == MonsterAbility.CHARGE ? (
-							((Monster)o1).getAttack() == ((Spell)o2).getAction().getPriority() ? (
+						((Monster)o1).getAttack() == ((Spell)o2).getAction().getPriority() ? (
 								((Monster)o1).getCost() >= ((Spell)o2).getCost() ? 1 : -1
-							) : ((Monster)o1).getAttack() > ((Spell)o2).getAction().getPriority() ? -1 : 1) : 1;
+						) : ((Monster)o1).getAttack() > ((Spell)o2).getAction().getPriority() ? -1 : 1) : 1;
 			}
 		} else if (o2 instanceof Spell){
 			return ((Spell)o1).getAction() == ((Spell)o2).getAction() ? ( o1.getCost() < o2.getCost() ? -1 : 1):
-				((Spell)o1).getAction().getPriority() > ((Spell)o2).getAction().getPriority() ? -1 : 1;
+					((Spell)o1).getAction().getPriority() > ((Spell)o2).getAction().getPriority() ? -1 : 1;
 		}
 
 		return ((Monster)o2).getMonsterAbility() == MonsterAbility.CHARGE ? (
